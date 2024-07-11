46e694f0be66385ca124520280358ea0dbb337f7
y-west24 <y.west.24.19@gmail.com>
2024-07-11 17:20:57 +0900

    チャットルームの削除機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,7 @@ class RoomsController < ApplicationController
 
   def index
   end
-  
+
   def new
     @room = Room.new
   end
@@ -12,8 +12,14 @@ class RoomsController < ApplicationController
     if @room.save
       redirect_to root_path
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
   end
 
   private
